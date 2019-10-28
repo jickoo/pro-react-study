77f2c448321bc7b2217b1cf1fcd7af6e8d1e29d6
jickoo <christiron@naver.com>
2019-10-28 19:00:03 +0900

    추가 작업

--- src/KanbanBoard/app/KanbanBoardContainer.js
+++ src/KanbanBoard/app/KanbanBoardContainer.js
@@ -98,6 +98,7 @@ class KanbanBoardContainer extends Component {
 		});
 	}
 
+	// TODO :: 연동 필요
 	toggleTask(cardId, taskId, taskIndex) {
 		let prevState = this.state;
 
@@ -167,6 +168,7 @@ class KanbanBoardContainer extends Component {
 		}
 	}
 
+	// TODO :: 연동 필요
 	persistCardDrag(cardId, status) {
 		let cardIndex = this.state.cards.findIndex((card) => card.id == cardId);
 		let card = this.state.cards[cardIndex]
